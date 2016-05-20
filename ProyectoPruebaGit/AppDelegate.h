2de6a8e87e7df2cd9bfc5f198a08cff412b7969c
Maximiliano Arciniega <maximilianoarciniega@macbook-pro-de-maximiliano.local>
2016-05-20 02:11:44 -0500

    Initial Commit

---
+++ ProyectoPruebaGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProyectoPruebaGit
+//
+//  Created by Maximiliano Arciniega on 20/05/16.
+//  Copyright © 2016 Maximiliano Arciniega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProyectoPruebaGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProyectoPruebaGit
+//
+//  Created by Maximiliano Arciniega on 20/05/16.
+//  Copyright © 2016 Maximiliano Arciniega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
